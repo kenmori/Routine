5b7f06dde03d2c95eec6fa5129cff364be655dc0
森田賢二 <kenjimoritata@gmail.com>
2018-06-09 12:41:15 +0900

    add routing

--- hybrid-app/App.js
+++ hybrid-app/App.js
@@ -1,6 +1,8 @@
 //React Native app entry point
 import React from 'react'
 import HybridApp from './src/App'
+import { Router, Switch, Route, Link } from './src/Routing/Routing'
+
 export default class NativeApp extends React.Component {
   render() {
     return <HybridApp />

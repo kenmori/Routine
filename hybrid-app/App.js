dbae42507fe9525a5ff0d67c4a24f8da3a8fdb7a
森田賢二 <kenjimoritata@gmail.com>
2018-06-10 10:26:37 +0900

    add redux

--- hybrid-app/App.js
+++ hybrid-app/App.js
@@ -1,10 +1,13 @@
 //React Native app entry point
 import React from 'react'
+//HybridAppとしているがその名前でexportしていない。動きを見るとどんな名前でもいい
 import HybridApp from './src/App'
 import { Router, Switch, Route, Link } from './src/Routing/Routing'
 
-export default class NativeApp extends React.Component {
+export default class  extends React.Component {
   render() {
-    return <HybridApp />
+    return (
+      <HybridApp />
+    )
   }
 }
---
+++ hybrid-app/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from '../Routing/Routing'
+import {FlatList,View, Text, StyleSheet } from 'react-native'
+
+const Header = () => (
+  <View>
+    <FlatList data={[{key: "about"}, {key: "scedule"}]} renderItem={({item}) =>
+        <Link to={`/${item.key}`}><Text>{item.key}</Text></Link>
+        }>
+      </FlatList>
+  </View>
+)
+
+export {Header}
